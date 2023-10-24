bc2d4de19fb5a41ebf92a4306a0212fb1626fa9b
Emma Paulovich <hpaulowicz@gmail.com>
2023-10-24 11:57:28 +0300

    Migrations path

--- app/functions/database.ts
+++ app/functions/database.ts
@@ -1,14 +1,14 @@
 import { DataSource } from "typeorm";
 import config from "../config";
 
-export const dataSource = new DataSource({
+const dataSource = new DataSource({
 	url: config.database.connectionString,
 	ssl: {
 		rejectUnauthorized: false,
 	},
 	type: "postgres",
 	entities: [],
-	migrations: [`${__dirname}/../../dist/migrations/*.{js}`],
+	migrations: [`${__dirname}/../../migrations/*.{ts,js}`],
 	migrationsTableName: "system_migrations",
 	logging: true,
 	synchronize: true,
